53124876c9f4e460b9ec9e08e3246af7baf929ac
nosmith18 <nosmith@wisc.edu>
2021-09-22 14:33:12 -0500

    Milestone 1 Commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,10 +1,34 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.TintTypedArray;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public void clickFunction(View view) {
+        Log.i("info", "Button Pressed!");
+
+        EditText inputText = (EditText) findViewById(R.id.textPersonName);
+        String str = inputText.getText().toString();
+
+        // Toast.makeText(MainActivity.this, inputText.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
